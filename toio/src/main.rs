c42f8f63eeb8fdf494b14011f73fa957c8b95991
Ramarko Bhattacharya <ramarkob@gmail.com>
2025-07-06 19:04:58 -0500

    Added Command Line Flags

--- toio/src/main.rs
+++ toio/src/main.rs
@@ -13,33 +13,77 @@ use std::sync::Arc;
 use std::time::SystemTime;
 use std::vec;
 
+use clap::Parser;
 use futures::future::join_all;
 use futures::future::Either::{Left, Right};
 use tokio::sync::RwLock;
 
-const SHOW_TERMINAL: bool = true;
-const ORDERED: bool = true;
-const USE_FILTER: bool = true;
+#[derive(Parser)]
+#[command(name = "toio")]
+struct Args {
+    /// Set receiving port
+    #[arg(short, long)]
+    port: Option<usize>,
+
+    /// Set remote port
+    #[arg(short, long)]
+    remote: Option<usize>,
+
+    /// Show terminal UI
+    #[arg(short, long)]
+    terminal: bool,
+
+    /// Use unfiltered search
+    #[arg(short, long)]
+    search: bool,
+
+    /// Use ordered search
+    #[arg(short, long)]
+    ordered: bool,
+
+    /// Filter toios by IDs (comma-separated list e.g. 1,2,3)
+    #[arg(short, long, value_delimiter = ',')]
+    axlab_id: Option<Vec<usize>>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let filter: Vec<usize> = vec![3, 100];
+    let args = Args::parse();
 
     // create scanner and array of toios
-    let scanner = match USE_FILTER {
-        true => ToioScanner::new_with_filter(ORDERED, filter.clone()).await?,
-        false => ToioScanner::new().await?,
+    let scanner = match args.axlab_id.clone() {
+        Some(filter) => {
+            if args.terminal {
+                if args.ordered {
+                    println!("Running Ordered Search for Toios: {:?}", filter.clone());
+                } else {
+                    println!("Running Unordered Search for Toios: {:?}", filter.clone());
+                }
+            }
+            ToioScanner::new_with_filter(args.ordered, filter.clone()).await?
+        }
+        None => {
+            if !args.search {
+                println!("You must provide IDs when not in search mode");
+                process::exit(0);
+            }
+
+            if args.terminal {
+                println!("Running Unfiltered Search for Toios");
+            }
+            ToioScanner::new().await?
+        }
     };
     // let scanner = ToioScanner::new_with_filter(true, vec![3, 100]).await?;
     let mut toios = scanner.search().await?;
     let connected: Arc<RwLock<Vec<Arc<RwLock<Toio>>>>> = Arc::new(RwLock::new(vec![]));
 
     // server and client address
-    let host_addr = "0.0.0.0:3334";
-    let to_addr = "0.0.0.0:3333";
+    let host_addr = format!("0.0.0.0:{}", args.port.unwrap_or(3334));
+    let to_addr = format!("0.0.0.0:{}", args.remote.unwrap_or(3333));
 
     // open socket and create buffer
-    let socket = Arc::new(UdpSocket::bind(host_addr).unwrap());
+    let socket = Arc::new(UdpSocket::bind(&host_addr)?);
     let mut buf = [0u8; rosc::decoder::MTU];
 
     // whenever a message is recieved through OSC, forward to toio
@@ -48,17 +92,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
     tokio::spawn(async move {
         // let mut now = SystemTime::now();
         while let Ok(size) = sock.recv(&mut buf) {
-            let (_, packet) = rosc::decoder::decode_udp(&buf[..size]).unwrap();
-            if let Some((toionum, cmd)) = handle_packet(packet) {
-                let connected_read = connected_clone.read().await;
-                if toionum < connected_read.len() {
-                    let toio = connected_read[toionum].read().await;
+            if let Ok((_, packet)) = rosc::decoder::decode_udp(&buf[..size]) {
+                if let Some((toionum, cmd)) = handle_packet(packet) {
+                    let connected_read = connected_clone.read().await;
+                    if toionum < connected_read.len() {
+                        let toio = connected_read[toionum].read().await;
 
-                    let last_command = toio.get_last_command();
-                    let mut last_command_write = last_command.write().await;
-                    *last_command_write = Some(SystemTime::now());
+                        let last_command = toio.get_last_command();
+                        let mut last_command_write = last_command.write().await;
+                        *last_command_write = Some(SystemTime::now());
 
-                    toio.toio.send_command(cmd).await;
+                        toio.toio.send_command(cmd).await;
+                    }
                 }
             }
         }
@@ -78,6 +123,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
                     // create instance of Toio to record toio info
                     let mut toio = Toio::new(toio_peripheral);
+                    if args.terminal {
+                        println!("Toio Connected: {}", toio.id);
+                    }
+
                     let battery = toio.get_battery();
                     let last_update = toio.get_last_update();
 
@@ -86,19 +135,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     let id = connected_write.len();
 
                     // start process to listen for messages from toio
-                    let toio_channel = tokio::spawn(async move {
-                        while let Some(update) = updates.next().await {
-                            // if it is a battery update, record it in the Toio
-                            if let Update::Battery { level } = update {
-                                let mut battery = battery.write().await;
-                                *battery = Some(level);
+                    let toio_channel = tokio::spawn({
+                        let to_addr = to_addr.clone();
+                        async move {
+                            while let Some(update) = updates.next().await {
+                                // if it is a battery update, record it in the Toio
+                                if let Update::Battery { level } = update {
+                                    let mut battery = battery.write().await;
+                                    *battery = Some(level);
+                                }
+
+                                // record time of update
+                                let mut last_update = last_update.write().await;
+                                *last_update = Some(SystemTime::now());
+
+                                send_packet(&sock, &to_addr, id, update);
                             }
-
-                            // record time of update
-                            let mut last_update = last_update.write().await;
-                            *last_update = Some(SystemTime::now());
-
-                            send_packet(&sock, to_addr, id, update);
                         }
                     });
 
@@ -116,10 +168,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     }))
                     .await;
 
+                    let toio_id = ids.iter().position(|id| *id == peripheral_id);
+
                     // Find the index of the matching peripheral_id
-                    if let Some(idx) = ids.iter().position(|id| *id == peripheral_id) {
-                        connected_write[idx].write().await.disconnect();
-                    }
+                    if let Some(idx) = toio_id {
+                        let mut toio = connected_write[idx].write().await;
+                        toio.disconnect();
+                        if args.terminal {
+                            println!("Toio Disconnected: {}", toio.id);
+                        }
+                    };
                 }
             }
         }
@@ -127,7 +185,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // // start TUI process
     let mut terminal: ToioUI = None;
-    if SHOW_TERMINAL {
+    if !args.terminal {
         terminal = setup_terminal()?;
     }
 
@@ -196,13 +254,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .await;
 
         //  update UI
-        let toio_filter = match USE_FILTER {
-            true => Some(filter.clone()),
-            false => None,
-        };
-
         if let Some(ref mut toio_ui) = terminal {
-            toio_ui.draw(ui(toio_info, toio_filter))?;
+            toio_ui.draw(ui(toio_info, args.axlab_id.clone()))?;
         }
 
         // // exit terminal if "Q" key is pressed
